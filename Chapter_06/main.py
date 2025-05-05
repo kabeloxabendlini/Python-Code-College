af70dcb2a1032ebda726491332f1055cc00921c6
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-05-05 14:43:41 +0200

    A basic overview of Chapter 06

--- Chapter_06/main.py
+++ Chapter_06/main.py
@@ -3,3 +3,7 @@ print(Kabelo_0)
 Kabelo_0['x_position'] = 0
 Kabelo_0['y_position'] = 25
 print(Kabelo_0)
+
+Kabelo_0 = {'Race': 'Black', 'Age': '19'}
+name_value = Kabelo_0.get('name_value', 'with a name value assigned.')
+print(name_value)
