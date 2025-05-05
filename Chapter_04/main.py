ff9b6e474dd3ba9d06c6a881f20f1d01b2a1c0f6
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-05-05 14:01:26 +0200

    python programming

---
+++ Chapter_04/main.py
@@ -0,0 +1,5 @@
+# 
+
+another_list = [5, 10, 15, 20, 25]
+
+print (another_list [2:4]) 
---
+++ Chapter_05/main.py
@@ -0,0 +1,29 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+for car in cars:
+ if car == 'bmw':
+  print(f"car.upper()")
+else:
+ print(f"car.title()")
+
+ my_age = 20
+
+if my_age > 20:
+     print("You must be older than 20.")
+elif my_age < 20:
+     print("You must be younger than 20.")
+
+else:
+     print("You are 20.")
+
+     var = 0
+
+# requested_topping = 'mushrooms'
+# if requested_topping != 'anchovies':
+#  print("Hold the anchovies.")
+
+#  age = 18
+#  age == 18
+
+     
+     
+    
