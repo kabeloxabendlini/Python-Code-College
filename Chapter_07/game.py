95110b24bba45b20fffdfcb4a2bb717257614f3a
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-05-05 16:40:51 +0200

    Modified Python file.

---
+++ Chapter_07/game.py
@@ -0,0 +1,12 @@
+lives = 5
+mission = True 
+game = ""
+
+game_is_active = True 
+
+while game_is_active:
+    
+    if lives == 0:
+        game_is_active: False
+    elif mission == False:
+        game_is_active = False
---
+++ Chapter_07/main.py
@@ -0,0 +1,28 @@
+# # full_name = input("What is your first name ? ")
+
+# # print(f"Hello{full_name}")
+
+# # full_name += input("What is your last name ?")
+
+# # print(f"Hello {full_name}")
+
+# age = 18
+
+# if int(age) < 18:
+#     print("Yes,your are an adult.")
+# elif int(age) < 18:
+#     print("Not today.")
+
+# number = 0
+
+# while number < 10:
+#     print(number)
+#     number += 1
+
+answer = input("Want to carry on ?").upper()
+
+while answer == "YES":
+    answer = input
+    number += 1
+
+    print("finished")
