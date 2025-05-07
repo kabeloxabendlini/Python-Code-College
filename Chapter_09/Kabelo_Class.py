cf569c6c1e565c05ac3966781a23a3221ee7f4bb
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-05-07 16:40:04 +0200

    Update Kabelo_Class.py

--- Chapter_09/Kabelo_Class.py
+++ Chapter_09/Kabelo_Class.py
@@ -11,7 +11,12 @@ class MyDog(Myself):
 
     def __init__(self, my_dog_name):
         super().__init__(my_dog_name)
+    
+    def greet(self):
+        print(f"Boom {self.name}")
 
 Pammy = MyDog("Pam")
+Sofie = MyDog("Sofie")
 
 print(Pammy.name)
+Sofie.greet()
